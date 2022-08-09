88a4c4ffc9f6f46d7fa66d68c4d8ca5cb83c8fcf
hbravo <hubravo13@gmail.com>
2022-08-09 17:21:49 -0400

    complete where statements Operators Operators Combiner

--- index.js
+++ index.js
@@ -1,29 +1,140 @@
+
+class OperatorCombiner {
+    #query;
+    constructor(query) { this.#query = query; }
+    AND(condition = null) {
+
+        let and = " AND ";
+        if (condition) and = and + condition;
+        this.#query = this.#query + and;
+        return new Operator(this.#query);
+    }
+    ANDNOT(condition = null) {
+
+        let andnot = " AND NOT ";
+        if (condition) andnot = andnot + condition;
+        this.#query = this.#query + andnot;
+        return new Operator(this.#query);
+    }
+    OR(condition = null) {
+        let or = " OR ";
+        if (condition) or = or + condition;
+        this.#query = this.#query + " OR ";
+        return new Operator(this.#query);
+    }
+    get() { return this.#query; }
+}
+class Operator {
+    #query;
+    constructor(query) { this.#query = query; }
+    get equ() {
+        this.#query = this.#query + " = ?";
+        return new OperatorCombiner(this.#query);
+    }
+    get nequ() {
+        this.#query = this.#query + " <> ?";
+        return new OperatorCombiner(this.#query);
+    }
+    get gthn() {
+        this.#query = this.#query + " > ?";
+        return new OperatorCombiner(this.#query);
+    }
+    get gtrOrequ() {
+        this.#query = this.#query + " >= ?";
+        return new OperatorCombiner(this.#query);
+    }
+    get lthn() {
+        this.#query = this.#query + " < ?";
+        return new OperatorCombiner(this.#query);
+    }
+    get lwrOrequ() {
+        this.#query = this.#query + " <= ?";
+        return new OperatorCombiner(this.#query);
+    }
+    get LIKE() {
+        this.#query = this.#query + " LIKE ? ";
+        return new Operator(this.#query);
+    }
+    get TRUE() {
+        this.#query = this.#query + " TRUE ";
+        return this;
+    }
+
+    IN(...values) {
+
+        if (!values) { Error("Not values provided"); return; }
+
+        this.#query = this.#query + ` IN (${values.join(", ")})`;
+        return new OperatorCombiner(this.#query);
+    }
+    NOTIN(...values) {
+
+        if (!values) { Error("Not values provided"); return; }
+
+        this.#query = this.#query + ` NOT IN (${values.join(", ")})`;
+        return new OperatorCombiner(this.#query);
+    }
+    ANY(sub_query) {
+
+        if (!sub_query) { Error("Not sub query provided"); return; }
+
+        this.#query = this.#query + ` ANY (${sub_query})`;
+        return new OperatorCombiner(this.#query);
+    }
+
+    ALL(sub_query) {
+
+        if (!sub_query) { Error("Not sub query provided"); return; }
+
+        this.#query = this.#query + ` ALL (${sub_query})`;
+        return new OperatorCombiner(this.#query);
+    }
+    
+    get BETWEEN(){ 
+
+        this.#query = this.#query + ` BETWEEN ? AND ?`;
+        return new OperatorCombiner(this.#query);
+
+    }
+    get NOTBETWEEN(){ 
+
+        this.#query = this.#query + ` NOT BETWEEN ? AND ?`;
+        return new OperatorCombiner(this.#query);
+
+    }
+
+}
 class WhereStatements {
     #query;
     constructor(query) { this.#query = query; }
     get() { return this.#query; }
+    WHERE(field) {
+        this.#query = this.#query + ` WHERE ${field}`
+        return new Operator(this.#query);
+    }
+    WHERENOT(field) {
+        this.#query = this.#query + ` WHERE NOT ${field}`
+        return new Operator(this.#query);
+    }
 }
 class From {
     #query;
     constructor(query) {
         this.#query = query + " FROM";
     }
-
     FROM(tableName) {
         if (!tableName) { Error("Table Name not provided"); return; }
         this.#query = this.#query + ` ${tableName}`;
         return new WhereStatements(this.#query);
     }
-
 }
 class Query {
     #query;
     constructor() { }
     SELECT(...fields) {
 
-        if (!fields) {Error(""); return};
-        if (typeof (fields) == "array") fields = fields.join(", ");
-        this.#query = `SELECT ${fields}`;
+        if (!fields) { Error("not fields provided"); return };
+        this.#query = `SELECT ${fields.join(", ")}`;
         return new From(this.#query);
 
     }
