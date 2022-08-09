9bd81be2336e69ed808fc5a252acdccaf1008a08
hbravo <hubravo13@gmail.com>
2022-08-09 12:57:23 -0400

    select function add From class Where statement class

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+class WhereStatements {
+    #query;
+    constructor(query) { this.#query = query; }
+    get() { return this.#query; }
+}
+class From {
+    #query;
+    constructor(query) {
+        this.#query = query + " FROM";
+    }
+
+    FROM(tableName) {
+        if (!tableName) { Error("Table Name not provided"); return; }
+        this.#query = this.#query + ` ${tableName}`;
+        return new WhereStatements(this.#query);
+    }
+
+}
 class Query {
     #query;
     constructor() { }
-    get() { return this.#query; }
-    SELECT() { }
+    SELECT(...fields) {
+
+        if (!fields) {Error(""); return};
+        if (typeof (fields) == "array") fields = fields.join(", ");
+        this.#query = `SELECT ${fields}`;
+        return new From(this.#query);
+
+    }
     INSERT() { }
     UPDATE() { }
     DELETE() { }
