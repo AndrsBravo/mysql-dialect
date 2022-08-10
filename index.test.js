464cdb61ffa3ca3e1269d5c2e43c6f39692b3d2e
hbravo <hubravo13@gmail.com>
2022-08-10 18:03:48 -0400

    test for references

--- index.test.js
+++ index.test.js
@@ -1,1 +1,10 @@
 const query = require("./index.js");
+
+describe("Declaration", () => {
+    it("Query object reference", () => { expect(query).toBeDefined() });
+    it("Query.SELECT function reference", () => { expect(query.SELECT).toBeDefined() });
+    it("Query.INSERT function reference", () => { expect(query.INSERT).toBeDefined() });
+    it("Query.UPDATE function reference", () => { expect(query.UPDATE).toBeDefined() });
+    it("Query.DELETE function reference", () => { expect(query.DELETE).toBeDefined() });
+});
+
